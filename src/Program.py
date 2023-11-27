8a29813d132c29a40331c7d568ef743af17f2f17
Zaid Shahzad <zaidshahzadbusiness@gmail.com>
2023-11-27 12:59:08 -0500

    Model setup and gradio setup

---
+++ src/Program.py
@@ -0,0 +1,11 @@
+import Model
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!"
+
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+if __name__ == "__main__":
+    demo.launch(show_api=False, share=True)

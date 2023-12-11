c1b77befe09db34465ffba68689c5a222f8e0086
Zaid Shahzad <zaidshahzadbusiness@gmail.com>
2023-12-11 17:52:23 -0500

    Improved Gradio interface

--- src/Model.py
+++ src/Model.py
@@ -2,25 +2,23 @@ import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split
 
-# ---------------------------------
 # Creating dataset and cleaning it
 dataset = pd.read_csv("./dataset/Heart_disease_cleveland_new.csv")
 
 dataset.isnull().sum()
 dataset['target'] = dataset['target'].replace({0: 'Normal', 1: 'Heart Disease'})
-# ---------------------------------
 
-
-# ------------------------
 # X, Y - TRAIN TEST SPLIT
 X = dataset.drop(['target'], axis=1)
 Y = dataset['target']
 
 datasets = train_test_split(X, Y, train_size=0.7, test_size=0.3, random_state=2)
 (X_train, X_test, Y_train, Y_test) = datasets
-# ------------------------
 
 model = tree.DecisionTreeClassifier(criterion='entropy', max_depth=4)
+model.fit(X_train, Y_train)
 
-def getModel():
-    return model
+def predict(age, sex, cp, thalach, exang, oldpeak, slope, ca, thal):
+    input_data = [[age, sex, cp, thalach, exang, oldpeak, slope, ca, thal]]
+    prediction = model.predict(input_data)
+    return prediction
--- src/Program.py
+++ src/Program.py
@@ -1,11 +1,27 @@
 import Model
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!"
+def predict_heart_disease(age, sex, cp, thalach, exang, oldpeak, slope, ca, thal):
+    prediction = Model.predict(age, sex, cp, thalach, exang, oldpeak, slope, ca, thal)
+    return "You are likely to have heart disease." if prediction[0] == 'Heart Disease' else "You are unlikely to have heart disease."
 
+questions = [
+    "Please enter the age of the patient:",
+    "What is the patient's birth gender? (Enter 0 for female, 1 for male):",
+    "What type of chest pain did the patient receive? (0: asymptomatic, 1: typical angina, 2: atypical angina, 3: non-anginal pain):",
+    "What is the patient's maximum heart rate during exercise?",
+    "Does the patient receive angina (chest pain) during exercise? (Enter 1 for Yes, 0 for No):",
+    "How much does the patient's heart activity decrease during exercise compared to rest? Please provide a numerical value.:",
+    "What is the patient's slope of the peak exercise ST segment? (0: downsloping, 1: flat, 2: upsloping):",
+    "How many of the patient's major vessels are colored by fluoroscopy? (Enter a number from 0 to 3):",
+    "Please enter the patient's thalassemia value. (1: normal, 2: fixed defect, 3: reversible defect):"
+]
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(
+    fn=predict_heart_disease,
+    inputs=[gr.Text(label=q) for q in questions],
+    outputs="text"
+)
 
 if __name__ == "__main__":
-    demo.launch(show_api=False, share=True)
+    demo.launch()
